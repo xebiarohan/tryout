205a1ba396b93c9057d78226b6e8eb1d68f9a446
Adriaan de Jonge <adriaandejonge@gmail.com>
2015-07-23 16:51:17 +0530

    Add test

--- src/main/java/in/tryout/Tryout.java
+++ src/main/java/in/tryout/Tryout.java
@@ -1,7 +1,14 @@
 package in.tryout;
 
+
+
 class Tryout {
   public Tryout() {
     super();
   }
+
+  public boolean returnTrue() {
+    return false;
+  }
+
 }
--- src/test/java/in/tryout/TryoutTest.java
+++ src/test/java/in/tryout/TryoutTest.java
@@ -1,5 +1,19 @@
 package in.tryout;
 
-class TryousTest {
+import static org.junit.Assert.assertEquals;
 
+class TryoutTest {
+
+    private Tryout instance;
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+        instance = new Tryout();
+
+    }
+
+    @org.junit.Test
+    public void testReturnTrue() throws Exception {
+        assertEquals(instance.returnTrue(), true);
+    }
 }
